d5ad9b3fa9c680f6ae7c0eb36ae939e477e01839
Andrey-Vyshegorodskiy <93198379+Andrey-Vyshegorodskiy@users.noreply.github.com>
2022-06-03 13:52:48 +0300

    after review 1.93

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', "testovy_key")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', os.getenv('HOST')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
